93f531b436e9d21ea6e37d2ebfa9efb877030f04
fathima hashem <fathima.hashem@northsouth.edu>
2022-02-13 00:18:50 +0600

    commit

---
+++ src/main/java/com/example/vinternship/VinternshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinternshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vinternship/VinternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
